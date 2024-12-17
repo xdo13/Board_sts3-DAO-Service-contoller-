033a06d9e41791e46c0f8729df74b11335c2b77f
xdo13 <xdo13@naver.com>
2024-12-17 17:54:12 +0900

    댓글 CRUD 테스트 완료

--- src/main/java/com/company/mapper/ReplyMapper.java
+++ src/main/java/com/company/mapper/ReplyMapper.java
@@ -4,5 +4,11 @@ import com.company.domain.ReplyVO;
 
 public interface ReplyMapper {
 	public int insert(ReplyVO vo);
+	public ReplyVO read(Long rno);
+	public int delete(Long rno);
+	
+	//reply, update만 수정가능
+	public int update(ReplyVO reply);
+	
 
 }
---
+++ src/test/java/com/company/mapper/ReplyMapperTests.java
@@ -0,0 +1,59 @@
+package com.company.mapper;
+
+import static org.junit.Assert.*;
+
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.company.domain.ReplyVO;
+
+import lombok.extern.log4j.Log4j;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class ReplyMapperTests {
+	
+	@Autowired
+	private ReplyMapper mapper;
+	
+	private Long[] bnoArr = { 63L, 62L, 58L, 57L, 56L };
+
+	@Test
+	public void testInsert() {
+		IntStream.rangeClosed(1, 10).forEach(i->{
+			ReplyVO vo = new ReplyVO();
+			vo.setBno(bnoArr[i % 5]);
+			vo.setReply("댓글 테스트" + i);
+			vo.setReplyer("replyer");
+			mapper.insert(vo);
+		});
+		
+	}
+	@Test
+	public void testRead() {
+		ReplyVO vo = mapper.read(6L);
+		log.info(vo);
+		
+	}
+	@Test
+	public void testDelete() {
+		int result = mapper.delete(3L);
+		log.info(result);
+	}
+	@Test
+	public void testUpdate() {
+		ReplyVO vo = new ReplyVO();
+		vo.setRno(4L);
+		vo.setReply("4번만 수정");
+		
+		mapper.update(vo);
+	}
+
+}
